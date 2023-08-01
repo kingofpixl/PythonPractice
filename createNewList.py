8dc7fc411cc89ded3bfbabe8d5329e7173e5630e
Wesley Waldern <wesley.waldern@uleth.ca>
2023-08-01 01:42:22 -0600

    finished program

--- createNewList.py
+++ createNewList.py
@@ -6,3 +6,11 @@ list1 = [10, 20, 25, 30, 35]
 list2 = [40, 45, 60, 75, 90]
 
 resultingList = []
+for num in list1:
+    if num % 2 == 1:
+        resultingList.append(num)
+for num in list2:
+    if num % 2 == 0:
+        resultingList.append(num)
+
+print(resultingList)
