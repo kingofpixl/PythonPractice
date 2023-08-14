3a49039596d40e0b36d6e3971c4e3fdfbe69cbbe
Wesley Waldern <wesley.waldern@uleth.ca>
2023-08-14 17:09:44 -0600

    checking if the car is started/stopped already

--- basicCarStatus.py
+++ basicCarStatus.py
@@ -1,16 +1,25 @@
 userInput = ""
+started = False
 while True:
     userInput = input("please type in a command (help, start, stop, quit) ").lower()
     if userInput == "help":
         print('''
-              start - to start the car
-              stop - to stop the car
-              quit - to exit the program
+start - to start the car
+stop - to stop the car
+quit - to exit the program
               ''')
     elif userInput == "start":
-        print("Car started...Ready to go!")
+        if started == False:
+            started = True
+            print("Car started...Ready to go!")
+        else:
+            print("Car is already Started")
     elif userInput == "stop":
-        print("Car stopped.")
+        if started == True:
+            started = False
+            print("Car stopped.")
+        else:
+            print("Car is already stopped")
     elif userInput == "quit":
         break
     else:
